a84589b644fae3227088c4c0026a418aa74e8f3a
john1340a <jean-kelly.seguin@outlook.fr>
2024-04-29 11:14:38 +0200

    Update User.js and SearchAnnonce.tsx files

--- Frontend/src/Pages/SearchAnnonce.tsx
+++ Frontend/src/Pages/SearchAnnonce.tsx
@@ -34,13 +34,13 @@ const SearchAnnonce = () => {
   const [logements, setLogements] = useState([]);
   const [error, setError] = useState<string | null>(null);
   const [responseData, setResponseData] = useState<any>(null);
-  const [isMaisonChecked, setIsMaisonChecked] = useState(true); // État pour la case à cocher maison
-  const [isAppartementChecked, setIsAppartementChecked] = useState(true); // État pour la case à cocher appartement
+  const [isMaisonChecked, setIsMaisonChecked] = useState(true);
+  const [isAppartementChecked, setIsAppartementChecked] = useState(true); 
 
   const handleSearch = async () => {
     try {
       let apiUrl = 'http://localhost:3000/api/annonce';
-
+  
       // Construire la requête API en fonction des filtres
       let params = [];
       if (ville) params.push(`address=${encodeURIComponent(ville)}`);
@@ -49,17 +49,21 @@ const SearchAnnonce = () => {
       if (isAppartementChecked) params.push('type=appartement');
       if (params.length > 0) apiUrl += `?${params.join('&')}`
       else apiUrl += '/';
-
+  
       const response = await axios.get(apiUrl);
       let filteredResults = response.data;
 
-      if (ville || prixMax || !isMaisonChecked || !isAppartementChecked) {
-        if (ville) filteredResults = filteredResults.filter((logement: any) => logement.address === ville);
+      // Filter results based on selected criteria
+      if (ville || prixMax || isMaisonChecked || isAppartementChecked) {
+        if (ville) {
+          const villeRegex = new RegExp(ville, 'i');
+          filteredResults = filteredResults.filter((logement: any) => villeRegex.test(logement.address));
+        }
         if (prixMax) filteredResults = filteredResults.filter((logement: any) => logement.price <= prixMax);
-        if (!isMaisonChecked) filteredResults = filteredResults.filter((logement: any) => logement.type !== 'maison');
-        if (!isAppartementChecked) filteredResults = filteredResults.filter((logement: any) => logement.type !== 'appartement');
+        if (isMaisonChecked) filteredResults = filteredResults.filter((logement: any) => logement.type === 'maison');
+        if (isAppartementChecked) filteredResults = filteredResults.filter((logement: any) => logement.type === 'appartement');
       }
-
+  
       if (filteredResults.length > 0) {
         setLogements(filteredResults);
         setError(null);
@@ -73,7 +77,7 @@ const SearchAnnonce = () => {
       setError('Erreur lors de la récupération des données');
       setResponseData(null);
     };
-  }
+  }  
 
   return (
     <Container>
@@ -116,20 +120,21 @@ const SearchAnnonce = () => {
               </Grid>
             )}
 
-            {responseData && (
-              <Grid item xs={12}>
-                <DataDisplay>
-                  {responseData.map((logement: any) => (
-                    <div key={logement._id}>
-                      <Typography variant="subtitle1">Title: {logement.title}</Typography>
-                      <Typography variant="subtitle1">Address: {logement.address}</Typography>
-                      <Typography variant="subtitle1">Price: {logement.price}</Typography>
-                      <hr />
-                    </div>
-                  ))}
-                </DataDisplay>
-              </Grid>
-            )}
+              {responseData && (
+                <Grid item xs={12}>
+                  <DataDisplay>
+                    {responseData.map((logement: any) => (
+                      <div key={logement._id}>
+                        <Typography variant="subtitle1">Title: {logement.title}</Typography>
+                        <Typography variant="subtitle1">Address: {logement.address}</Typography>
+                        <Typography variant="subtitle1">Price: {logement.price}</Typography>
+                        <hr />
+                      </div>
+                    ))}
+                  </DataDisplay>
+                </Grid>
+              )}
+
           </Grid>
         </CardContent>
       </StyledCard>
